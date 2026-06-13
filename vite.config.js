fc6f1a727847d146b6545d9266a0791698918c25
itxskbaloch4-jpg <itxskbaloch4@gmail.com>
2026-06-13 08:30:50 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'frontend',           // <-- add this
+  root: '.',
   build: {
-    outDir: '../dist',        // <-- output to root dist/ for Vercel
+    outDir: 'dist',
     emptyOutDir: true,
   },
 })
